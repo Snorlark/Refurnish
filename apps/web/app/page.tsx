c9ee2d9d3f56bc8f68d8f381e4107c1e8b0b2b16
Angelique Anne Valdez <iamxngelique@gmail.com>
2025-08-28 04:08:21 +0800

    Product Catalog Initial Commit

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import Image from "next/image";
 import Link from 'next/link';
 import { useEffect, useRef } from 'react';
@@ -348,15 +349,14 @@ export default function Home() {
 
       <div className="border-t-[0.2px] border-(--color-olive) mt-12  text-center"></div>
 
-
-      {/* Footer */}
+{/* footer */}
       <footer className="bg-(--color-white) text-(--color-primary) py-16 px-6 lg:px-16">
-        <div className="container mx-auto">
-          <div className="grid grid-cols-1 md:grid-cols-4 gap-12">
+        <div className="max-w-7xl px-6 lg:px-9 py-4 container mx-auto">
+          <div className="grid grid-cols-1 md:grid-cols-4 gap-10">
             {/* Logo and Social */}
             <div>
-                <img src="/refurnishlogoSection.png" alt="Logo" className=" w-80 pb-2 h-auto object-center" />
-              <div className="flex space-x-6">
+                <img src="/refurnishlogoSection.png" alt="Logo" className=" w-60 pb-2 h-auto object-center" />
+              <div className="flex space-x-3">
                 <div className="w-12 h-12 rounded-full bg-(--color-white) flex items-center justify-center hover:bg-(--color-olive) hover:text-(--color-white) transition-modern cursor-pointer">
                   <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 24 24">
                     <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z"/>
@@ -382,8 +382,8 @@ export default function Home() {
 
             {/* Information */}
             <div>
-              <h4 className="text-md font-semibold mb-6">Information</h4>
-              <ul className="space-y-4">
+              <h4 className="text-md font-semibold mb-4">Information</h4>
+              <ul className="space-y-3 text-sm">
                 <li><a href="#" className="hover:text-(--color-olive) transition-modern text-sm">Privacy</a></li>
                 <li><a href="#" className="hover:text-(--color-olive) transition-modern text-sm">Terms of Use</a></li>
                 <li><a href="#" className="hover:text-(--color-olive) transition-modern text-sm">About Us</a></li>
@@ -392,8 +392,8 @@ export default function Home() {
 
             {/* Contact */}
             <div>
-              <h4 className="text-md font-semibold mb-6">Contact Us</h4>
-              <ul className="space-y-4 text-sm">
+              <h4 className="text-md font-semibold mb-4">Contact Us</h4>
+              <ul className="space-y-3 text-sm">
                 <li>Email: info@refurnish.com</li>
                 <li>Mobile/Viber: +63 912 345 6789</li>
                 <li>Messenger: @refurnish</li>
